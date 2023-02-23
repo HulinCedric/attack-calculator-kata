ab3af12ed8f78c696729ce81b054654f8abf61c8
Cédric Hulin <hulin.cedric@gmail.com>
2023-02-23 19:00:01 +0100

    refactor: Naming

--- csharp/Game.tests/CharacterAttackShould.cs
+++ csharp/Game.tests/CharacterAttackShould.cs
@@ -7,64 +7,69 @@ namespace Game.tests;
 public class CharacterAttackShould
 {
     [Fact]
-    public void Should_not_inflict_damage_when_attack_roll_is_less_or_equal_than_defender_armor_class()
+    public void Be_missed_when_attack_roll_is_less_or_equal_than_defender_armor_class()
     {
         // Arrange
-        const int attackRoll = 6;
+        const int diceRoll = 6;
         var attacker = Human().WithForce(2).WithWeaponDamage(3).Build();
 
         var defender = Orc().WithArmorClass(8).Build();
 
         // Act
-        var damage = attacker.Attack(defender, attackRoll);
+        var damage = attacker.Attack(defender, diceRoll);
 
         // Assert
         damage.Should().Be(0);
     }
 
     [Fact]
-    public void Should_inflict_weapon_damage_when_attack_roll_is_greater_than_defender_armor_class()
+    public void Be_hit_when_attack_roll_is_greater_than_defender_armor_class()
     {
         // Arrange
-        const int attackRoll = 10;
-        var attacker = Human().WithWeaponDamage(3).Build();
+        const int weaponDamage = 3;
+
+        const int diceRoll = 10;
+        var attacker = Human().WithWeaponDamage(weaponDamage).Build();
 
         var defender = Orc().WithArmorClass(8).Build();
 
         // Act
-        var damage = attacker.Attack(defender, attackRoll);
+        var damage = attacker.Attack(defender, diceRoll);
 
         // Assert
-        damage.Should().Be(3);
+        damage.Should().Be(weaponDamage);
     }
 
     [Fact]
-    public void Should_inflict_double_damage_when_attack_roll_is_20()
+    public void Inflict_double_damage_when_dice_roll_is_20()
     {
         // Arrange
-        const int attackRoll = 20;
-        var attacker = Human().WithForce(2).WithWeaponDamage(3).Build();
+        const int weaponDamage = 3;
+        const int doubleDamage = 6;
+
+        const int diceRoll = 20;
+        var attacker = Human().WithForce(2).WithWeaponDamage(weaponDamage).Build();
 
         var defender = Orc().WithArmorClass(8).Build();
 
         // Act
-        var damage = attacker.Attack(defender, attackRoll);
+        var damage = attacker.Attack(defender, diceRoll);
 
         // Assert
-        damage.Should().Be(6);
+        damage.Should().Be(doubleDamage);
     }
 
     [Fact]
-    public void Should_not_inflict_damage_when_attack_roll_is_1()
+    public void Inflict_no_damage_when_dice_roll_is_1()
     {
         // Arrange
-        const int attackRoll = 1;
+        const int diceRoll = 1;
         var attacker = Human().WithForce(4).WithWeaponDamage(3).Build();
 
         var defender = Orc().WithArmorClass(4).Build();
 
         // Act
-        var damage = attacker.Attack(defender, attackRoll);
+        var damage = attacker.Attack(defender, diceRoll);
 
         // Assert
         damage.Should().Be(0);
