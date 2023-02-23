dbe3ca09a3d9c3638770cbd5b1e18d0d9a813a49
Cédric Hulin <hulin.cedric@gmail.com>
2023-02-23 17:26:28 +0100

    test: Normal weapong damage rule

--- csharp/Game.tests/CharacterAttackShould.cs
+++ csharp/Game.tests/CharacterAttackShould.cs
@@ -6,16 +6,32 @@ namespace Game.tests;
 public class CharacterAttackShould
 {
     [Fact]
-    public void Should_return_zero_when_attack_is_less_or_equal_than_defender_armor_class()
+    public void Should_not_inflict_damage_when_attack_is_less_or_equal_than_defender_armor_class()
     {
-        var attacker = new Character(0, 3, "human", 5);
-        var defender = new Character(10, 0, "orc", 0);
-        var dice = new DeterministicDice(5);
+        var attacker = new Character(0, 3, "human", 2);
+        var defender = new Character(8, 0, "orc", 0);
+        var dice = new DeterministicDice(6);
 
         // Act
         var damage = attacker.Attack(defender, dice);
 
         // Assert
         damage.Should().Be(0);
+    }
+
+
+    [Fact]
+    public void Should_inflict_weapon_damage_when_attack_is_greater_than_defender_armor_class()
+    {
+        // Arrange
+        var attacker = new Character(0, 3, "human", 2);
+        var defender = new Character(8, 0, "orc", 0);
+        var dice = new DeterministicDice(10);
+
+        // Act
+        var damage = attacker.Attack(defender, dice);
+
+        // Assert
+        damage.Should().Be(3);
     }
 }
